064904bd5c95a20170f3a059fb419e9debb3bc84
MatijaZoricRed <matija.zoric@redbudi.com>
2026-03-17 14:57:25 +0100

    New change menu and add sosevi

--- _pages/Sauces/Products.js
+++ _pages/Sauces/Products.js
@@ -3,65 +3,65 @@ import { useState } from "react";
 
 const Products = () => {
   const [products, setProducts] = useState([
-    {
-      id: 1,
-      name: "Garlic",
-      amount: "250ml",
-      image: "/Sauces/List/garlic.png",
-      color: "#d033ca",
-    },
-    {
-      id: 2,
-      name: "Lemon & Herb",
-      amount: "250ml",
-      image: "/Sauces/List/lemon.png",
-      color: "#f4e900",
-    },
+    // {
+    //   id: 1,
+    //   name: "Garlic",
+    //   amount: "250ml",
+    //   image: "/Sauces/List/garlic.png",
+    //   color: "#d033ca",
+    // },
+    // {
+    //   id: 2,
+    //   name: "Lemon & Herb",
+    //   amount: "250ml",
+    //   image: "/Sauces/List/lemon.png",
+    //   color: "#f4e900",
+    // },
     {
       id: 3,
-      name: "Green Chilli",
+      name: "Lemon & Herb",
       amount: "250ml",
       image: "/Sauces/List/chili.png",
       color: "#88c619",
     },
-    {
-      id: 4,
-      name: "BBQ Piri-Piri",
-      amount: "250ml",
-      image: "/Sauces/List/bbq.png",
-      color: "#f16005",
-    },
+    // {
+    //   id: 4,
+    //   name: "BBQ Piri-Piri",
+    //   amount: "250ml",
+    //   image: "/Sauces/List/bbq.png",
+    //   color: "#f16005",
+    // },
     {
       id: 5,
-      name: "BBQ",
+      name: "MILD",
       amount: "250ml",
       image: "/Sauces/List/bbq2.png",
       color: "#f59200",
     },
-    {
-      id: 6,
-      name: "Mild",
-      amount: "250ml",
-      image: "/Sauces/List/mild.png",
-      color: "#4ecddf",
-    },
-    {
-      id: 7,
-      name: "Hot",
-      amount: "250ml",
-      image: "/Sauces/List/hot.png",
-      color: "#e50490",
-    },
-    {
-      id: 8,
-      name: "Premium Reserve",
-      amount: "250ml",
-      image: "/Sauces/List/premium.png",
-      color: "#d90010",
-    },
-    {
+    // {
+    //   id: 6,
+    //   name: "Mild",
+    //   amount: "250ml",
+    //   image: "/Sauces/List/mild.png",
+    //   color: "#4ecddf",
+    // },
+    // {
+    //   id: 7,
+    //   name: "Hot",
+    //   amount: "250ml",
+    //   image: "/Sauces/List/hot.png",
+    //   color: "#e50490",
+    // },
+    // {
+    //   id: 8,
+    //   name: "Premium Reserve",
+    //   amount: "250ml",
+    //   image: "/Sauces/List/premium.png",
+    //   color: "#d90010",
+    // },
+     {
       id: 9,
-      name: "XXX Hot",
+      name: "Hot",
       amount: "250ml",
       image: "/Sauces/List/xxxhot.png",
       color: "#e40313",
@@ -90,10 +90,10 @@ const Products = () => {
                 <img
                   src={product.image}
                   alt={product.name}
-                  className={`object-contain rounded-md transition-all duration-300 hover:scale-110`}
+                  className={`object-contain rounded-md transition-all duration-300 hover:scale-105`}
                 />
               </div>
-              <h1 id={product?.id} className={`text-xl text-center uppercase -mt-9 font-medium`}>
+              <h1 id={product?.id} className={`text-xl text-center uppercase mt-4 font-medium`}>
                 {product.name}
               </h1>
             </div>
